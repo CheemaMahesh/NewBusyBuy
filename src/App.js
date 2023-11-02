1c63dc3810ed0b2262431388ab0b70d3a0946553
Cheema Mahesh <maheshbabucheema789@gmail.com>
2023-11-02 15:43:46 +0530

    2nd Update

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ import Home from './Components/Pages/Home';
 
 function App() {
 
-function callHandle(){
-  fetch('https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.2513844&lng=81.62964130000002&restaurantId=194638')
-  .then(res=>res.json())
-  .then(json=>console.log(json))
-}
+
 
   return (
     <div className="App">
-      <button onClick={callHandle}>Vlick mee i say</button>
       <Home/>
       <Contact/>
 
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -10,7 +10,30 @@ export default function Home(){
                             <div className={styles.dLeftT}>FLAT &#8377;400 OFF</div>
                             <div className={styles.dLeftb}> ON YOUR FIRST ORDER +FREE SHIPPING*</div>
                     </div>
-                    <div className={styles.dRight}></div>
+                    <div className={styles.dRight}>
+                        <div className={styles.rightT}>
+                                CODE:
+                        </div>
+                        <div className={styles.rightB}>
+                                BUSYBUY400
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.festivalDiv}>
+                    <div className={styles.LeftW}>
+                    </div>
+                    <div className={styles.saleDiv}>
+                        <div className={styles.saleDivT}>
+                                    India's Biggest<br/>
+                                    Diwali Dhamaka
+                        </div>
+                        <div className={styles.saleDivB}>
+                                    50-90% OFF
+                        </div>
+
+                    </div>
+                    <div className={styles.RightM}>
+                    </div>
                 </div>
         </div>
     )
